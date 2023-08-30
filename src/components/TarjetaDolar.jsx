c9ec2734ba397771fe313625f4d2c5573f57e85f
joeecasa <joeecasa@gmail.com>
2023-08-30 13:49:11 +0200

    responsive

--- src/components/TarjetaDolar.jsx
+++ src/components/TarjetaDolar.jsx
@@ -3,20 +3,24 @@ import "./components.css"
 
 
 
-const TarjetaDolar = ({nombre,venta,compra,logo}) => {
+const TarjetaDolar = ({ nombre, venta, compra, logo }) => {
 
 
 
     return (
         <div className="card div-card">
-                <div className="card-body">
+            <div className="card-body">
+                <div className='card-title-container'>
                     <h5 className="card-title">{nombre}
-                    <img src={logo} alt="logo" className='card-logo' />
+                        <img src={logo} alt="logo" className='card-logo' />
                     </h5>
+                </div>
+                <div className='card-text-container'>
                     <p className="card-text">Compra {compra}</p>
                     <p className="card-text">Venta {venta}</p>
                 </div>
-        </div>
+            </div>
+        </div >
     )
 }
 
